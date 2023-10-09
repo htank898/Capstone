da8e8259b4ce705a4fa3f61998baeffee9b0cf88
htank898 <htank898@gmail.com>
2023-10-09 10:18:23 -0500

    git add Week7 HW update

--- store/Links.js
+++ store/Links.js
@@ -3,5 +3,6 @@ export default [
   { title: "About", text: "About" },
   { title: "House", text: "House of Reps" },
   { title: "Senate", text: "Senators" },
-  { title: "Trade", text: "Mock Trade" }
+  { title: "Trade", text: "Mock Trade" },
+  { title: "News", text: "News" }
 ];
