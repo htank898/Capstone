052150e6bc450dc2cd87e45a0649717a345c03b4
htank898 <htank898@gmail.com>
2023-10-09 21:06:21 -0500

    add Nav Bar Update

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,4 @@
 import html from "html-literal";
 // import Links from "../store/Links";
 
-export default links => html`
-  <nav>
-    <i class="fas fa-bars"></i>
-    <ul class="hidden--mobile nav-links">
-      ${links
-        .map(
-          link =>
-            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
-        )
-        .join("")}
-    </ul>
-  </nav>
-`;
+export default links => html``;
