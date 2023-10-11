f147f95c10367b3ffd132104c616a5c01c67acbe
htank898 <htank898@gmail.com>
2023-10-11 18:07:45 -0500

    update for MT review

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,11 +1,13 @@
 import html from "html-literal";
 
 export default state => html`
-  <h1>Trade like the 1%</h1>
-
   <h3>
     The weather in ${state.weather.city} is ${state.weather.description}.
     Temperature is ${state.weather.temp}F, and it feels like
     ${state.weather.feelsLike}F.
   </h3>
+  <div>
+    Trade like the 1%
+    <div></div>
+  </div>
 `;
--- store/Links.js
+++ store/Links.js
@@ -1,8 +1,8 @@
 export default [
   { title: "Home", text: "Home" },
-  { title: "House", text: "House of Reps" },
-  { title: "Senate", text: "Senators" },
-  { title: "Trade", text: "Mock Trade" },
+  { title: "House", text: "House" },
+  { title: "Senate", text: "Senate" },
+  { title: "Trade", text: "Trade" },
   { title: "News", text: "News" },
   { title: "About", text: "About" }
 ];
