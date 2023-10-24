ff8cad8af83fe81715cdcffb20e849561b92f39d
htank898 <htank898@gmail.com>
2023-10-24 11:21:02 -0500

    added content to news page

--- components/views/News.js
+++ components/views/News.js
@@ -1,3 +1,26 @@
 import html from "html-literal";
 
-export default state => html``;
+export default state => html`
+  <section>
+    <h1>
+      Lobbying
+    <h2 id="lobbyHeader">
+      Lobbying, in its current form, undermines democracy by granting undue
+      power to corporations while sidelining the interests of voters. Wealthy
+      corporations wield influence through financial resources, shaping policies
+      that prioritize profits over the public good. The revolving door between
+      government and lobbying firms blurs the line between public service and
+      corporate interests. The secretive nature of lobbying hides these
+      influences from voters, eroding transparency and accountability. To
+      protect the democratic process, urgent measures are needed to enhance
+      transparency, impose stricter regulations, and prioritize policies that
+      prioritize the welfare of the people over corporate gains.
+    </h2>
+  </section>
+  <section>Beaglepup</section>
+  <section>Beaglepup</section>
+  <section>Beaglepup</section>
+  <img src=
+  https://as1.ftcdn.net/v2/jpg/05/05/93/08/1000_F_505930860_ztlXWR1HogCAwZ2B8X1HBd2JGT1qH2sc.jpg
+  alt="Description of the image" id="bribe"> Poopballs
+`;
